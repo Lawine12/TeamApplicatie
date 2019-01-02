1c94d0999c9466c0095221e9ffd96f2f87902396
Raymond <rkok12@gmail.com>
2019-01-02 20:43:25 +0100

    added edit function

---
+++ TeamsApplicatie/DataViewExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamsApplicatie
+{
+    public static class DataViewExtensions
+    {
+        public static List<string> ToStringList(this DataView view)
+        {
+            var list = new List<string>();
+            foreach (DataRowView item in view)
+            {
+                list.Add(item.Row[0].ToString());
+            }
+            return list;
+        }
+    }
+}
--- TeamsApplicatie/EditMatchForm.xaml.cs
+++ TeamsApplicatie/EditMatchForm.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +24,7 @@ namespace TeamsApplicatie
     {
         private readonly string _id;
         private DataTable _matchData;
+        private DataSet _teams;
 
         public EditMatchForm()
         {
@@ -32,6 +34,8 @@ namespace TeamsApplicatie
         public EditMatchForm(string id) : this()
         {
             _id = id;
+            loadCombo1();
+            loadCombo2();
             LoadData();
         }
 
@@ -42,7 +46,7 @@ namespace TeamsApplicatie
 
         private void cancel_Click(object sender, RoutedEventArgs e)
         {
-
+            Close();
         }
 
         private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -61,8 +65,8 @@ namespace TeamsApplicatie
             if (matchData.Rows.Count == 1)
             {
                 var row = _matchData.Rows[0];
-                comboBoxEditTeam1.Text = (string)row["TeamName1"];
-                comboBoxEditTeam2.Text = (string)row["TeamName2"];
+                comboBoxEditTeam1.SelectedItem = (string)row["TeamName1"];
+                comboBoxEditTeam2.SelectedItem = (string)row["TeamName2"];
                 EditMatchDatePicker.Text = row["MatchDate"].ToString();
             }
         }
@@ -79,6 +83,35 @@ namespace TeamsApplicatie
                 sqlAdapter.Fill(_matchData);
             }
             return _matchData;
+        }
+
+        public void loadCombo1()
+        {
+            using (var connection = DatabaseHelper.OpenDefaultConnection())
+            {
+                var querystring = "SELECT TeamName FROM TeamData";
+
+                var cmd = new SqlCommand(querystring, connection);
+                var dataAdapter = new SqlDataAdapter(cmd);
+                _teams = new DataSet();
+                dataAdapter.Fill(_teams, "TeamData");
+                comboBoxEditTeam1.ItemsSource = _teams.Tables[0].DefaultView.ToStringList();
+            }
+        }
+
+
+        public void loadCombo2()
+        {
+            using (var connection = DatabaseHelper.OpenDefaultConnection())
+            {
+                var querystring = "SELECT TeamName FROM TeamData";
+
+                var cmd = new SqlCommand(querystring, connection);
+                var dataAdapter = new SqlDataAdapter(cmd);
+                _teams = new DataSet();
+                dataAdapter.Fill(_teams, "TeamData");
+                comboBoxEditTeam2.ItemsSource = _teams.Tables[0].DefaultView.ToStringList();
+            }
         }
     }
 }
